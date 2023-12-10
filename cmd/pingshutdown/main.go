ed29c0596a895c05725492b689508a718981bdef
Morgan Helton <mhelton@gmail.com>
2023-12-10 15:43:36 -0600

    pinging in blocks

--- cmd/pingshutdown/main.go
+++ cmd/pingshutdown/main.go
@@ -4,19 +4,43 @@ import (
 	"fmt"
 	"github.com/prometheus-community/pro-bing"
 	"time"
+	"flag"
 )
 
 func main() {
-	pinger, _ := probing.NewPinger("www.google.com")
+	target := flag.String("t", "www.google.com", "target to begin shutdown when unavailable")
 
-	go func() {
-		pinger.Run()
-	}()
+	flag.Parse()
 
-	go func() {
-		time.Sleep(3 * time.Second)
-		fmt.Println(pinger.Statistics())
-	}()
+	c := make(chan bool)
+
+	go func(c chan bool) {
+		for {
+			pinger, _ := probing.NewPinger(*target)
+			pinger.Count = 10
+			pinger.Timeout = 10 * time.Second
+			pinger.Run()
+			if pinger.Statistics().PacketLoss == 100 {
+				fmt.Println("all pings failed") 
+			} else {
+				fmt.Println("some pings succeeded")
+			}
+		}
+	}(c)
+
+	go func(c chan bool) {
+		shutdownTimer := time.NewTimer(3 * time.Minute)
+
+	}(c)
+
+	// go func() {
+	// 	for {
+	// 		time.Sleep(2 * time.Second)
+	// 		fmt.Println(pinger.Statistics())
+
+	// 	}
+
+	// }()
 
 	select {}
 }
