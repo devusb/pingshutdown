f0f19450d44344855500465ea1b86160db5a814f
Morgan Helton <mhelton@gmail.com>
2023-12-10 21:52:14 -0600

    add custom countdown package

--- cmd/pingshutdown/main.go
+++ cmd/pingshutdown/main.go
@@ -5,42 +5,35 @@ import (
 	"github.com/prometheus-community/pro-bing"
 	"time"
 	"flag"
+	"github.com/devusb/pingshutdown/internal/countdown"
 )
+
+func shutdown() {
+	fmt.Println("shutting down system!")
+}
 
 func main() {
 	target := flag.String("t", "www.google.com", "target to begin shutdown when unavailable")
 
 	flag.Parse()
 
-	c := make(chan bool)
+	shutdownTimer := countdown.Countdown{Duration: 15*time.Second}
 
-	go func(c chan bool) {
+	go func() {
 		for {
 			pinger, _ := probing.NewPinger(*target)
-			pinger.Count = 10
-			pinger.Timeout = 10 * time.Second
+			pinger.Count = 5
+			pinger.Timeout = 5 * time.Second
 			pinger.Run()
 			if pinger.Statistics().PacketLoss == 100 {
-				fmt.Println("all pings failed") 
+				fmt.Println("all pings failed")
+				shutdownTimer.StartAfterFunc(shutdown)	
 			} else {
 				fmt.Println("some pings succeeded")
+				shutdownTimer.Stop()
 			}
 		}
-	}(c)
-
-	go func(c chan bool) {
-		shutdownTimer := time.NewTimer(3 * time.Minute)
-
-	}(c)
-
-	// go func() {
-	// 	for {
-	// 		time.Sleep(2 * time.Second)
-	// 		fmt.Println(pinger.Statistics())
-
-	// 	}
-
-	// }()
+	}()
 
 	select {}
 }
---
+++ internal/countdown/countdown.go
@@ -0,0 +1,46 @@
+package countdown
+
+import (
+	"time"
+)
+
+type Countdown struct {
+	endTime time.Time
+	Duration time.Duration
+	runningStatus bool
+	timer *time.Timer
+}
+
+func (c *Countdown) Start() {
+	if !c.runningStatus {
+		c.timer = time.NewTimer(c.Duration)
+		c.endTime = time.Now().Add(c.Duration)
+		c.runningStatus = true
+	}
+}
+
+func (c *Countdown) StartAfterFunc(f func()) {
+	if !c.runningStatus {
+		c.timer = time.AfterFunc(c.Duration, f)
+		c.endTime = time.Now().Add(c.Duration)
+		c.runningStatus = true
+	}
+}
+
+func (c *Countdown) Stop() {
+	if c.runningStatus {
+		c.Stop()
+		c.endTime = time.Now()
+		c.runningStatus = false
+	}
+}
+
+func (c *Countdown) EndTime() time.Time {
+	return c.endTime
+}
+
+func (c *Countdown) Status() bool {
+	return c.runningStatus
+}
+
+
